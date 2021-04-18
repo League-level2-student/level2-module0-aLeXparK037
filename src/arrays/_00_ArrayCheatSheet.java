b39d1b329f52a1cd073439c9cd8e6d867086d227
aLeXparK037 <65091873+aLeXparK037@users.noreply.github.com>
2021-04-17 17:56:13 -0700

    3

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
@@ -21,16 +23,36 @@ public class _00_ArrayCheatSheet {
 
 		
 		//6. make an array of 50 integers
-		String [] manyInts = new String [50];
+		Integer [] manyInts = new Integer [50];
 		
 		//7. use a for loop to make every value of the integer array a random number
 		for (int i = 0; i < manyInts.length; i++) {
-			
+			Random ran = new Random();
+			int num = ran.nextInt(50);
+			manyInts[i] = num;
 		}
 		//8. without printing the entire array, print only the smallest number in the array
-		
+/*int small = manyInts[0];
+		for (int i = 0; i < manyInts.length; i++) {
+			System.out.println(manyInts[i]);
+			if (small > manyInts[i]) {
+				small = manyInts[i];
+			}
+
+		}
+		System.out.println(small + " is the smallest number");*/
+
 		//9 print the entire array to see if step 8 was correct
 		
 		//10. print the largest number in the array.
+		int big = manyInts[0];
+		for (int i = 0; i < manyInts.length; i++) {
+			System.out.println(manyInts[i]);
+			if (big < manyInts[i]) {
+				big = manyInts[i];
+			}
+
+		}
+		System.out.println(big + " is the biggest number");
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,16 +1,35 @@
 package arrays;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
+public static void main(String[] args) {
+	Robot [] rob = new Robot [5];
 
 	//2. create an array of 5 robots.
 
 	//3. use a for loop to initialize the robots.
-
+	for (int i = 0; i < rob.length; i++) {
+		rob[i] = new Robot();
+	}
+int xaxis = 100;
 		//4. make each robot start at the bottom of the screen, side by side, facing up
+	for (int i = 0; i < rob.length; i++) {
+	rob[i].setX(xaxis);
+	rob[i].setY(500);
+	xaxis += 150;
+	}
 
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
+	for (int i = 0; i < rob.length; i++) {
+		Random ran = new Random();
+		int num = ran.nextInt(50);
+		rob[i].move(num);
+	}
 
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
@@ -19,4 +38,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
